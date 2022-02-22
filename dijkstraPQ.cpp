e96d27e668f39f0b497bdbae4612e4d579f262cf
Debapriya Chandra <77222446+debapriya36@users.noreply.github.com>
2022-02-23 00:56:53 +0530

    Minimun dist from source node to all other

---
+++ dijkstraPQ.cpp
@@ -0,0 +1,38 @@
+// Dijkstra Algorithm : 
+// minimun dis from source node to other nodes .
+
+const ll N=2e5;
+vector<ll>dis(N,1e18);
+
+void Dijkstra_PQ(ll source, ll n)
+{
+    
+    priority_queue<pair<ll,ll>,vector<pair<ll,ll>>,greater<pair<ll,ll>>> pq;
+
+    pq.push({source,0});
+    dis[source]=0;
+
+    while(pq.size())
+    {
+        auto node=pq.top();
+        pq.pop();
+        ll val=node.F;
+        ll dis=node.S;
+
+        for(auto it: adj[node])
+        {
+            ll wt=it.S;
+            ll Node=it.F;
+            if(dis[Node]>dis[node]+wt)
+            {
+                dis[Node]=dis[node]+wt;
+                pq.push({Node,dis[Node]});
+            }
+        }
+    }
+
+    for(ll i=0;i<n;i++)cout<<dis[i]<<" ";
+    cout<<endl;
+
+
+}
